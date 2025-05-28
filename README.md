aaf8282f14efa46fb68b7845a9796ee2a350dc41
Tiffany Lui <tiflst1020@gmail.com>
2025-05-28 10:19:08 +1000

    Added comments for javascript functions

--- README.md
+++ README.md
@@ -11,6 +11,13 @@ https://blog.hubspot.com/website/css-fade-in
 
 Learning how to create an Accordion for 'How It Works' dropdown box/content for Q&A:
 https://www.youtube.com/watch?v=dPLHi7tsoFU
+https://www.w3schools.com/howto/howto_js_accordion.asp
 
 Horizontal scrolling using Javascript:
 https://www.youtube.com/watch?v=gzXyRa7jwk4&t=107s
+
+Use of Clamping for the Hero-Slogan:
+https://developer.mozilla.org/en-US/docs/Web/CSS/clamp
+
+Document: querySelectorAll() method learnt
+https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let backBtn = document.getElementById("backBtn");
 let forwardBtn = document.getElementById("nextBtn");
 
 
-//The click event listener for the back button allows the user to scroll back 
+//The click event listener for the back & next button allows the user to scroll back 
 //& forth by 900 pixels. The behavior is set to smooth to create a smooth scrolling effect.
+
 backBtn.addEventListener("click", (event) => {
-    event.preventDefault();
+    event.preventDefault(); //added to prevent it from navigating back to a href link #
     scrollContainer.scrollBy({
         left: -900,
         behavior: "smooth"
@@ -24,15 +25,3 @@ nextBtn.addEventListener("click", (event) => {
         behavior: "smooth"
     })
 });
-
-/*
-Implementation of accordion in How It Works Page
-Allows the contentBx section to expand or collapse when clicked.
-*/
-const accordion = document.getElementsByClassName('contentBx'); //when contentBx clicked
-    for (let i = 0; i < accordion.length; i++) { //loops through each element in the accordion section
-        accordion[i].addEventListener('click', function() { //adds a click event listener for each element
-            this.classList.toggle('active'); //'this' is the clicked element, toggles the item to active
-            //it also removes the actie class if it is already there
-        });
-}
